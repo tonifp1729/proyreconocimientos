e1bbc46462a71804c81466e82009dec60e560aad
Antonio Manuel Figueroa Pinilla <tonifp1729@gmail.com>
2024-05-03 10:23:48 +0200

    Cambio a la estructura de ficheros. Archivos de configuración y conexión añadidos.

---
+++ src/config/config.php
@@ -0,0 +1,7 @@
+<?php
+    //Credenciales de conexión con la base de datos
+    DEFINE('DB_HOST', '002daw.proyectos.esvirgua.com');
+    DEFINE('DB_USER', 'proyectosevg_2daw00');
+    DEFINE('DB_PASSWORD', 'v+D[]e9p;gj2');
+    DEFINE('DB_NAME', 'proyectosevg_2daw00_BD1');
+?>
